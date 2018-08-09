bd9c6b5b09eb582cfbb70c3d9b8f22ef6c4c2a2d
junam20-meet <junam20@meet.mit.edu>
2018-08-09 03:31:14 -0400

    Stopping here

---
+++ juna_mini_proj.py
@@ -0,0 +1,197 @@
+import turtle
+import random #We'll need this later in the lab
+
+turtle.tracer(1,0) #This helps the turtle move more smoothly
+
+SIZE_X=800
+SIZE_Y=500
+turtle.setup(SIZE_X, SIZE_Y) #Curious? It's the turtle window  
+                             #size. 
+turtle.penup()
+
+SQUARE_SIZE = 20
+START_LENGTH = 6
+
+#Initialize lists
+pos_list = []
+stamp_list = []
+food_pos = []
+food_stamps = []
+
+#Set up positions (x,y) of boxes that make up the snake
+snake = turtle.clone()
+snake.shape("square")
+
+#Hide the turtle object (it's an arrow - we don't need to see it)
+turtle.hideturtle()
+for i in range (7):
+    x_pos=snake.pos ()[0]
+    y_pos=snake.pos ()[1]
+    x_pos+=SQUARE_SIZE
+
+    my_pos=(x_pos,y_pos)
+    snake.goto(x_pos,y_pos)
+    stamp_list.append (my_pos)
+    stamp_id = snake.stamp()
+    stamp_list.append (stamp_id)
+    print (snake.pos ())
+    
+UP_ARROW = "Up" #Make sure you pay attention to upper and lower 
+                #case
+LEFT_ARROW = "Left" #Pay attention to upper and lower case
+DOWN_ARROW = "Down" #Pay attention to upper and lower case
+RIGHT_ARROW = "Right" #Pay attention to upper and lower case
+TIME_STEP = 100 #Update snake position after this many 
+                #milliseconds
+SPACEBAR = "space" # Careful, it's not supposed to be capitalized!
+
+UP = 0
+#1. Make variables LEFT, DOWN, and RIGHT with values 1, 2, and 3
+####WRITE YOUR CODE HERE!!
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+direction = UP
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
+
+def up():
+    global direction
+    direction = UP
+    move_snake()
+    print ("you pressed the upkey")
+    
+turtle.onkeypress(up, UP_ARROW)
+
+def left():
+    global direction
+    direction = LEFT
+    move_snake()
+    print ("you pressed the left key")
+
+turtle.onkeypress(left,LEFT_ARROW)
+
+def right():
+    global direction
+    direction = RIGHT
+    move_snake()
+    print ("you pressed the right key")
+
+turtle.onkeypress(right,RIGHT_ARROW )
+
+def down():
+    global direction
+    direction = DOWN
+    move_snake()
+    print ("you pressed the down key")
+
+turtle.onkeypress(down,DOWN_ARROW)
+turtle.listen()
+
+def move_snake():
+    my_pos = snake.pos()
+    x_pos = my_pos[0]
+    y_pos = my_pos[1]
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+    if new_x_pos >= RIGHT_EDGE:
+        print("You hit the right edge! Game over")
+        quit()
+    elif new_x_pos <= LEFT_EDGE:
+        print("You hit the left edge! Game over!")
+        quit()
+    elif new_y_pos <= DOWN_EDGE:
+        print("You hit the down edge! Game over!")
+        quit()
+    elif new_y_pos >= UP_EDGE:
+        print("You hit the up edge! Game over!")
+        quit()
+    
+
+    if direction==RIGHT:
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print("You moved right!")
+    elif direction==LEFT:
+        snake.goto(x_pos - SQUARE_SIZE, y_pos)
+        print("You moved left!")
+    elif direction==DOWN:
+        snake.goto(x_pos, y_pos - SQUARE_SIZE)
+        print("YOU MOVED DOWN ")
+    elif direction==UP:
+        snake.goto(x_pos ,y_pos + SQUARE_SIZE)
+        print ("you moved up")
+    elif new_y_pos <= DOWN_EDGE:
+        print("You hit the down edge! Game over!")
+        quit()
+        global food_stamps, food_pos
+    #If snake is on top of food item
+    if snake.pos() in food_pos:
+        food_ind=food_pos.index(snake.pos()) #What does this do?
+        food.clearstamp(food_stamps[food_ind]) #Remove eaten food                 
+                                               #stamp
+        food_pos.pop(food_ind) #Remove eaten food position
+        food_stamps.pop(food_ind) #Remove eaten food stamp
+        print("You have eaten the food!")
+
+    
+    if pos_list[-1] in pos_list[:-1]:
+        print('you ate yourself')
+        quit()
+
+        
+    
+    
+
+    my_pos=snake.pos() 
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last 
+    #piece of the tail
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+
+        
+    my_pos=snake.pos() 
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last 
+    #piece of the tail
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+
+x=0
+
+turtle.register_shape("trash.gif" )
+food = turtle.clone()
+food.shape("trash.gif") 
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+for this_food_pos in food_pos:
+    food.goto (food_pos[x])
+    food_stamp=food.stamp()
+    food_stamps.append(food_stamp)
+    x = x+1
+
+def make_food():
+    min_x=-int(SIZE_X/2/SQUARE_SIZE)+1
+    max_x=int(SIZE_X/2/SQUARE_SIZE)-1
+    min_y=-int(SIZE_Y/2/SQUARE_SIZE)-1
+    max_y=int(SIZE_Y/2/SQUARE_SIZE)+1
+    food_x = random.randint(min_x,max_x)*SQUARE_SIZE
+    food_y = random.randint(min_y,max_y)*SQUARE_SIZE
+    food.goto (food_x,food_y)
+    food_pos.append ((food_x,food_y))
+    food_stamp = food.stamp()
+    food_stamp.apped (food_stamp)
+
+move_snake()
